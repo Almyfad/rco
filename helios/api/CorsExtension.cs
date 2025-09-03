8d9432537cb7de73d9c0229ac135cb74281f936d
Lary SENE <lary.sene@canal-plus.com>
2025-09-02 22:12:47 -0400

    helios : cors for dev

--- helios/api/CorsExtension.cs
+++ helios/api/CorsExtension.cs
@@ -4,21 +4,34 @@ public static class CorsExtension
     {
         col.AddCors(o =>
         {
-                o.AddPolicy("rco.org",
-                 policy =>
-                 {
-                     policy
-                     .SetIsOriginAllowed(origin => new Uri(origin).Host.EndsWith(".rose-croix-d-or.org"))
-                           .AllowAnyHeader()
-                           .AllowCredentials()
-                           .AllowAnyMethod();
-                 });
+            o.AddPolicy("rco.org",
+             policy =>
+             {
+                 policy
+                 .SetIsOriginAllowed(origin => new Uri(origin).Host.EndsWith(".rose-croix-d-or.org"))
+                       .AllowAnyHeader()
+                       .AllowCredentials()
+                       .AllowAnyMethod();
+             });
+            o.AddPolicy("localhost",
+             policy =>
+             {
+                 policy
+                 .WithOrigins("http://localhost:4200") // ou le port de ton front Angular
+                       .AllowAnyHeader()
+                       .AllowCredentials()
+                       .AllowAnyMethod();
+             });
         });
         return col;
     }
 
+
     public static IApplicationBuilder UseAppicationAngularCors(this WebApplication app)
     {
+        if (app.Environment.IsDevelopment())
+            return app.UseCors("localhost");
         return app.UseCors("rco.org");
+
     }
 }
