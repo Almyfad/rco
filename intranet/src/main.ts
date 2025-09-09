52ada59ec35394f8c10612df6dc43534bfc15036
Lary SENE <lary.sene@canal-plus.com>
2025-09-09 00:49:46 -0400

    syncfusion license key

--- intranet/src/main.ts
+++ intranet/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+registerLicense('Ngo9BigBOggjHTQxAR8/V1JFaF5cXGRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXZeeHVURWBdWER+XUdWYEg=');
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
